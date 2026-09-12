c3da660837961498354798a0eb872e735487516d
luiscarlos <luiscarlos15987@gmail.com>
2026-09-12 16:18:59 -0300

    feat: adiciona novas categorias e menu interativo

--- main.py
+++ main.py
@@ -106,20 +106,19 @@ def analisar_gastos():
      print("RECOMENDAÇÃO: Tente reduzir seus gastos para aumentar sua reserva.")
     else:
      print("SITUAÇÃO: Você possui uma margem razoável do salário disponível.")
-    while True:
-     print("\n===== MENU =====")
-     print("1 - Analisar gastos")
-     print("2 - Sair")
+while True:
+    print("\n===== MENU =====")
+    print("1 - Analisar gastos")
+    print("2 - Sair")
 
-     opcao = input("Escolha uma opção: ")
+    opcao = input("Escolha uma opção: ")
 
-     if opcao == "1":
+    if opcao == "1":
         analisar_gastos()
 
-     elif opcao == "2":
+    elif opcao == "2":
         print("Programa encerrado.")
         break
 
-     else:
-      print("ERRO: Opção inválida.")
-analisar_gastos()
+    else:
+        print("ERRO: Opção inválida.")
